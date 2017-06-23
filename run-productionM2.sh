d7085c42a186a785ad03a0993987c3cadc8143c2
Tassanai Yeeton <tassanai.yeeton@gmail.com>
2017-06-23 19:15:06 +0700

    Add bash file for deploy

--- run-productionM2.sh
+++ run-productionM2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker rm -f serviceswarm_nginx_1 serviceswarm_express_1 serviceswarm_mysql_1
-docker rmi -f serviceswarm_nginx serviceswarm_express serviceswarm_mysql
+docker rmi -f tassanai/serviceswarm_nginx tassanai/serviceswarm_express tassanai/serviceswarm_mysql
 docker pull tassanai/serviceswarm_nginx
 docker pull tassanai/serviceswarm_express
 docker pull tassanai/serviceswarm_mysql
